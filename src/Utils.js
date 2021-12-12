d749ed65d62262ebd647fcc1179255c1c782b009
ironhiro <iuncehiro@gmail.com>
2021-12-13 02:26:48 +0900

    change sort algorithm: sortByPublished

--- src/Utils.js
+++ src/Utils.js
@@ -8,28 +8,33 @@ export function getCurrentDate(separator='/'){
     return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
 }
 
+export function sortById(a,b){
+    if (a.id < b.id) return -1;
+    if (a.id > b.id) return 1;
+    return 0;
+}
 
 
 export function sortByPublished(a,b){
-    if(a.releaseCond == b.releaseCond){return sortByDate(a,b);} return a.releaseCond > b.releaseCond ? -1 : 1;
+    if(a.releaseCond == b.releaseCond){return sortByDate(a,b);} return a.releaseCond > b.releaseCond ? 1 : -1;
 }
 
 export function sortByDifficulties(a,b,difficulty){
     switch(difficulty){
         case 'easy':
-            if(a.difficulties.easy == b.difficulties.easy){return sortByDate(a,b);} return a.difficulties.easy > b.difficulties.easy ? 1 : -1;
+            if(a.difficulties.easy == b.difficulties.easy){return sortByPublished(a,b);} return a.difficulties.easy > b.difficulties.easy ? 1 : -1;
             break;
         case 'normal':
-            if(a.difficulties.normal == b.difficulties.normal){return sortByDate(a,b);} return a.difficulties.normal > b.difficulties.normal ? 1 : -1;
+            if(a.difficulties.normal == b.difficulties.normal){return sortByPublished(a,b);} return a.difficulties.normal > b.difficulties.normal ? 1 : -1;
             break;
         case 'hard':
-            if(a.difficulties.hard == b.difficulties.hard){return sortByDate(a,b);} return a.difficulties.hard > b.difficulties.hard ? 1 : -1;
+            if(a.difficulties.hard == b.difficulties.hard){return sortByPublished(a,b);} return a.difficulties.hard > b.difficulties.hard ? 1 : -1;
             break;
         case 'expert':
-            if(a.difficulties.expert == b.difficulties.expert){return sortByDate(a,b);} return a.difficulties.expert > b.difficulties.expert ? 1 : -1;
+            if(a.difficulties.expert == b.difficulties.expert){return sortByPublished(a,b);} return a.difficulties.expert > b.difficulties.expert ? 1 : -1;
             break;
         case 'master':
-            if(a.difficulties.master == b.difficulties.master){return sortByDate(a,b);} return a.difficulties.master > b.difficulties.master ? 1 : -1;
+            if(a.difficulties.master == b.difficulties.master){return sortByPublished(a,b);} return a.difficulties.master > b.difficulties.master ? 1 : -1;
             break;
     }
 
