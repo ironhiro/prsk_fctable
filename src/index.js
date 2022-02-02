e157ceb79f57c99257764cbeec1f24eab4d886ac
ironhiro <iuncehiro@gmail.com>
2022-02-02 16:40:51 +0900

    change structure of the js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './client/Root';
 import 'bootstrap/dist/css/bootstrap.css';
-
-
-
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Root />,
   document.getElementById('root')
 );
+//registerServiceWorker();
